cd23ffc19872e83754ea65660778a0131e9a6840
OMI-2921 <OM.Panchal@r-pac.com>
2026-08-22 16:03:49 +0530

    Refactor order form output check to use main function

--- tools/order_form_output_check.py
+++ tools/order_form_output_check.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
-st.title("ORDER-FORM TO OUTPUT CHECK")
 
-st.write("This tool will compare the selected Order Form data against the PDF output.")
+def main():
+
+    st.title("ORDER-FORM TO OUTPUT CHECK")
+
+    st.write(
+        "This tool will compare the selected Order Form "
+        "data against the PDF output."
+    )
